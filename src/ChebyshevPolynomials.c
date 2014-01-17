fee9f2dcdbbbdef2b68ef48ad8b3cafa755630c7
Brian Friesen <friesen@ou.edu>
2014-01-17 16:37:05 -0600

    Added error messages to Chebyshev polynomials for negative index.

--- src/ChebyshevPolynomials.c
+++ src/ChebyshevPolynomials.c
@@ -1,11 +1,21 @@
 #include <math.h>
 
-double cheby_poly_1(unsigned int n, double x)
+#include <cctk.h>
+
+double cheby_poly_1(int n, double x)
 {
+    if (n < 0){
+        CCTK_VError(__LINE__, __FILE__, CCTK_THORNSTRING,
+                "Invalid Chebyshev polynomial (1st kind) index: %d", n);
+    }
     return (cos((double)n * acos(x)));
 }
 
-double cheby_poly_2(unsigned int n, double x)
+double cheby_poly_2(int n, double x)
 {
+    if (n < 0){
+        CCTK_VError(__LINE__, __FILE__, CCTK_THORNSTRING,
+                "Invalid Chebyshev polynomial (2nd kind) index: %d", n);
+    }
     return (sin((double)(n+1) * acos(x)) / sin(acos(x)));
 }
