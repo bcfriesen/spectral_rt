08fdd519c1f7a317ff226280f4f8db9a835fbfcd
Jeremy Lusk <Jeremy.Lusk@Gmail.com>
2014-01-20 21:03:27 -0600

    Defined pi properly, removed main() function

--- src/GaussLobatto.c
+++ src/GaussLobatto.c
@@ -1,22 +1,11 @@
 #include <math.h>
-#include <stdio.h>
 
 // Returns the kth Gauss-Lobatto interpolation point
 double gauss_lobatto_point(int k, int N)
 {
-    double x_k = - cos(k * M_PI / N);
+    double pi = acos(-1.0);
+    
+    double x_k = - cos(k * pi / N);
 
     return x_k;
-}
-
-int main(int argc, char *argv[])
-{
-    int k = 1;
-    int N = 4;
-
-    double test_point = gauss_lobatto_point(k, N);
-
-    printf("test_point = %f\n", test_point);
-
-    return 0;
 }
