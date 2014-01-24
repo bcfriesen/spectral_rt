b7687586521300a35121e076f975626371cf32f2
Brian Friesen <friesen@ou.edu>
2014-01-23 22:01:09 -0600

    Changed Chebyshev polynomials from trigonometric to recursive formulae.
    The trigonometric identity of the Chebyshev polynomial of the second kind
    is undefined at x = +/-1 unless you use L'Hopital's rule. But computers
    don't do limits very well, so let's just change to a formalism that works
    for all values in [-1, +1].

--- src/ChebyshevPolynomials.c
+++ src/ChebyshevPolynomials.c
@@ -4,18 +4,47 @@
 
 double cheby_poly_1(int n, double x)
 {
-    if (n < 0){
+    int i;
+    double T_n, T_nm1, T_nm2;
+
+    if (n < 0) {
         CCTK_VError(__LINE__, __FILE__, CCTK_THORNSTRING,
                 "Invalid Chebyshev polynomial (1st kind) index: %d", n);
+    } else if (n == 0) {
+        return (1.0);
+    } else if (n == 1) {
+        return (x);
+    } else {
+        T_nm2 = 1.0;
+        T_nm1 = x;
+        for (i = 2; i <= n; i++) {
+            T_n = 2.0*x*T_nm1 - T_nm2;
+            T_nm2 = T_nm1;
+            T_nm1 = T_n;
+        }
+        return (T_n);
     }
-    return (cos((double)n * acos(x)));
 }
 
 double cheby_poly_2(int n, double x)
 {
-    if (n < 0){
+    int i;
+    double U_n, U_nm1, U_nm2;
+    if (n < 0) {
         CCTK_VError(__LINE__, __FILE__, CCTK_THORNSTRING,
                 "Invalid Chebyshev polynomial (2nd kind) index: %d", n);
+    } else if (n == 0) {
+        return (1.0);
+    } else if (n == 1) {
+        return (2.0 * x);
+    } else {
+        U_nm2 = 1.0;
+        U_nm1 = 2.0 * x;
+        for (i = 2; i <= n; i++) {
+            U_n = 2.0 * x * U_nm1 - U_nm2;
+            U_nm2 = U_nm1;
+            U_nm1 = U_n;
+        }
+        return (U_n);
     }
-    return (sin((double)(n+1) * acos(x)) / sin(acos(x)));
 }
